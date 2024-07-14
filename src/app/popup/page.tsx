768dcafe8dcbe5a6b8f9bec9febc3b4f0879bfd9
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-14 17:56:47 +0100

    initial commit

--- src/app/popup/page.tsx
+++ src/app/popup/page.tsx
@@ -1,22 +1,37 @@
-import React from 'react';
-import styles from '../styles/popup.module.css';
+'use client'
+import type { NextPage } from "next";
+import styles from "../styles/airdrop.module.css"
+import BottomNav from "../components/BottomNav";
+import { FaCopy } from "react-icons/fa6";
+import TopHeader from "../components/TopHeader";
+import ImageWithRays from "../components/ImageWIthRays";
 
-interface ImageWithRaysProps {
-  src: string;
-  alt: string;
-}
-
-const PopUp: React.FC<ImageWithRaysProps> = ({ src, alt }) => {
+const Airdrop: NextPage = () => {
+    
   return (
-    <div className={styles.container}>
-      <div className={styles.rays}>
-        {Array.from({ length: 12 }).map((_, i) => (
-          <div key={i} className={styles.ray}></div>
-        ))}
-      </div>
-      <img src={src} alt={alt} className={styles.image} />
-    </div>
+    <>
+      <div className={styles.airdrop}>
+        <TopHeader />
+        <ImageWithRays src="/frdlogo300.png" alt="image"/>
+
+        <div className={styles.airdropc}>
+            <div className={styles.header}>
+                <h2>Airdrop</h2>
+                <h3>Share in the community airdrop rewards</h3>
+            </div>
+            
+            <div className={styles.airdroplink}>
+                <div className={styles.airdropbtns}>
+                    <button className={styles.sharebtn}>
+                        Connect Wallet
+                    </button>
+                </div>
+            </div>
+        </div>
+        <BottomNav />
+      </div>      
+    </>
   );
 };
 
-export default PopUp;
+export default Airdrop;
