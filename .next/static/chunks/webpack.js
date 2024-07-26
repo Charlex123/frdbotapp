f342ad4b8c658f294802be6ddd86e8232e3b8d17
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-26 11:57:26 +0100

    another commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "f3a9f1e96c0e0bea"; }
+/******/ 		__webpack_require__.h = function() { return "3b55a92f2af3b615"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
