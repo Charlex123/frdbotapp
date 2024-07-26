332b395a201adb8fdaddaf30a5bf1d2f438dfe88
Charles Muoka <charlesmuoka1@gmail.com>
2024-07-26 08:48:19 +0100

    another commit

--- src/app/mine/page.tsx
+++ src/app/mine/page.tsx
@@ -4,6 +4,7 @@ import LicenseContainer from "./components/MineLicenseContainer";
 import FrameComponent from "./components/MineFrameComponent";
 import styles from "../styles/mine.module.css";
 import MineTabs from "./components/MineTabs";
+import { useAppUser } from "../provider";
 import Header from "../components/Header";
 import BottomNav from "../components/BottomNav";
 import DailyCombo from "./components/DailyCombo";
@@ -11,7 +12,9 @@ import TapComponent from "../components/TapComponent";
 import Points from "../components/Points";
 
 const Mine: NextPage = () => {
-  
+  const { appuser } = useAppUser();
+
+
   return (
     <div className={styles.minemain}>
       {/* <img className={styles.unionIcon} alt="" src="/union.svg" /> */}
@@ -19,7 +22,7 @@ const Mine: NextPage = () => {
       <div className={styles.minec}>
         <DailyCombo />
         <div className={styles.mined}>
-          <Points points={459000} />
+          <Points points={appuser!.dailypoints} />
           <LicenseContainer />
           <MineTabs />
           <TapComponent />
